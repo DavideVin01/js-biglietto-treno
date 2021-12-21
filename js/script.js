0c086333519f2c1007f03f56a94ea5ccdd4dd4bd
DavideVin01 <davidevinciguerradv01@gmail.com>
2021-12-21 18:18:27 +0100

    Adds discounted prices for underages & over 65

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ console.log('JS OK');
 // Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// va applicato uno sconto del 20% per i minorenni
+// va applicato uno discount del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
@@ -18,12 +18,30 @@ console.log('JS OK');
 */
 
 // #1 - #2
-const userKm = parseInt(prompt('Quanti chilometri devi percorrere?').trim());
-const userAge = parseInt(prompt('Quanti anni hai?').trim());
+let userKm = parseInt(prompt('Quanti chilometri devi percorrere?').trim());
+let userAge = parseInt(prompt('Quanti anni hai?').trim());
 
-console.log(`I chilometri da percorrere sono: ${userKm}`);
-console.log(`L'utente ha: ${userAge} anni`);
+console.log(`I chilometri da percorrere sono ${userKm}km`);
+console.log(`L'utente ha ${userAge} anni`);
+
 
 // #3
 
-// const prezzoTotale = 0
+let costoTotale = userKm + userAge;
+console.log(`Il prezzo del biglietto senza sconti è di ${costoTotale}€`);
+
+//#3.1
+// TODO 0.21 € al km
+
+// #3.2 - #3.3
+let prezzoScontato = 0;
+
+if (0 < userAge && userAge < 18) {
+    prezzoScontato = .20;
+    costoTotale = costoTotale - (costoTotale * prezzoScontato);
+    console.log(`Il prezzo del biglietto per i minorenni è di ${costoTotale}€`)
+} else if (userAge >= 65) {
+    prezzoScontato = .40;
+    costoTotale = costoTotale - (costoTotale * prezzoScontato);
+    console.log(`Il prezzo del biglietto per gli over 65 è di ${costoTotale}€`)
+}
